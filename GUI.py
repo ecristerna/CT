751b50811e0786f9b571a9e48cc2a1e0d59c68a5
Eduardo Cristerna <eduardo@schoolhub.com>
2016-04-28 23:21:51 -0500

    save n execute

--- GUI.py
+++ GUI.py
@@ -17,13 +17,17 @@ data = ""
 def executeFile():
 	global fileName
 
+	saveFile()
+
 	VM.run(fileName)
 
 def saveFile():
-	inputProgram = T.get('1.0', END)
+	global fileName
 
-	data_file = open(fileName)
-	data_file.write(asksaveasfile(inputProgram))
+	data = T.get('1.0', END)
+
+	data_file = open(fileName, 'w')
+	data_file.write(data)
 	data_file.close()
 
 def loadFile():
@@ -34,8 +38,6 @@ def loadFile():
 	data_file = open(askopenfilename())
 	fileName = data_file.name
 
-	T2.delete('1.0', END)
-	T2.insert(END, data_file.name)
 	root.wm_title("CT - " + data_file.name)
 
 	data = data_file.read()
@@ -46,10 +48,10 @@ def loadFile():
 
 root = Tk()
 root.wm_title("CT")
+
 S = Scrollbar(root)
-T = Text(root, height = 50, width = 100)
-T2 = Text(root, height = 50, width = 100)
-T2.insert(END, 'HOLA')
+T = Text(root, height = 30, width = 100)
+
 button = Button(root, text = 'Execute', width = 25, command = executeFile)
 button2 = Button(root, text = 'Save', width = 25, command = saveFile)
 button3 = Button(root, text = 'Load', width = 25, command = loadFile)
@@ -59,7 +61,6 @@ button3.pack()
 
 S.pack(side = RIGHT, fill = Y)
 T.pack(side = LEFT, fill = Y)
-T2.pack(side = RIGHT, fill = Y)
 S.config(command = T.yview)
 T.config(yscrollcommand = S.set)
 
